596d89eddc5c9b45cbf94bd21d511cd0d698ff8a
azultasul <azultasul@gmail.com>
2023-02-23 18:28:01 +0900

    모듈 작업 중

--- shuffleText.js
+++ shuffleText.js
@@ -1,188 +1,162 @@
-const sayAuto = [
-  "Hello",
-  "Yo!",
-  "¡Hola!",
-  "’Sup!",
-  "G’day",
-  "Aloha",
-  "Salut",
-  "Ciao",
-  "안녕하세요",
-  "こんにちは",
-]
-const sayHover = [
-  "Hello",
-  "안녕하세요",
-]
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ShuffleText = factory());
+})(this, (function () { 'use strict';
 
-const hoverTarget = document.querySelector(".shuffle-event-target")
-const initData = { // 사용자 설정
-  textArray: sayAuto,
-  playType: 'hover', // 'auto' / 'hover' / 'click'
-  stayTime: 1500, 
-  replaceTime: 100, 
-  isDisorder: true, // true / false
-  isRandom: true, // true / false
-  hoverTarget: hoverTarget
-}
+  /**
+   * ShuffleText 
+   * @author Tasul, azultasul@gmail.com
+   */
 
-const data = {
-  say: [],
-  txtLength: [],
-  currentSay: [],
-  nextText: [],
-  replaceIndex: [],
-  playIndex: [],
-  stayTime: 1500,
-  replaceTime: 100,
-  totalReplaceTime: 0,
-};
+  let settings = {}
+  
+  const data = {
+    say: [],
+    txtLength: [],
+    currentSay: [],
+    nextText: [],
+    replaceIndex: [],
+    playIndex: [],
+    totalReplaceTime: 0,
+  };
 
-let setReplaceTime;
-const replaceText = (sayArray) => {
-  // console.log("replaceText");
-  data.playIndex.forEach((text, index) => {
-    setReplaceTime = setTimeout(() => {
-      data.currentSay[text] = `<span>${data.nextText[text]}</span>`
-      
-      if (index == (data.playIndex.length - 1)) {
-        data.currentSay = data.currentSay.filter((el) => el !== '<span></span>')
-      }
-      insertHtml();
-    }, data.replaceTime * (index))
-  })
-}
+  function ShuffleText(target, options) {
+    settings = {
+      shuffleTarget: options.shuffleTarget || null,
+      playType: options.playType || null,
+      isReplacedRandomly: options.isReplacedRandomly || true,
+      isOrderedArray: options.isOrderedArray || false,
+      stayTime: options.stayTime || 1500,
+      replaceTime: options.replaceTime || 100,
+      shuffleTarget: document.querySelector(`${target}`),
+    }
+    data.say = settings.isOrderedArray ? shuffleArray(options.textArray) : options.textArray;
+    data.txtLength = data.say.map(item => item.length);
+  
+    data.say[0].split('').forEach(txt => {
+      data.currentSay.push(`<span>${txt}</span>`);
+    })
 
-const setShortArray = (shortArray, longArray) => {
-  // console.log("setShortArray");
-  let shortI = 0
-  let i
-  for (i = 0; i < longArray.length; i++) {
-    if (data.replaceIndex[shortI] !== i) {
-      shortArray.splice(i, 0, '');
-    } else {
-      shortI++
+    insertHtml(settings.shuffleTarget);
+    if (settings.playType == 'auto') {
+      playAuto();
     }
   }
-}
 
-const getRandomIndex = (short, long) => {
-  // console.log("getRandomIndex");
-  // replace Index
-  let check
-  let replaceI
-  let lastReplaceI = -1;
-  let i
-  for (i = 0; i < short; i++) {
-    check = long - (short - i) - (lastReplaceI + 1)
-    replaceI = Math.floor(Math.random() * check);
-    lastReplaceI = (lastReplaceI + 1) + replaceI;
-    data.replaceIndex.push(lastReplaceI);
+  let setReplaceTime;
+  const replaceText = () => {
+    clearTimeout(setReplaceTime)
+    data.playIndex.forEach((text, index) => {
+      setReplaceTime = setTimeout(() => {
+        data.currentSay[text] = `<span>${data.nextText[text]}</span>`
+        
+        if (index == (data.playIndex.length - 1)) {
+          data.currentSay = data.currentSay.filter((el) => el !== '<span></span>')
+        }
+        insertHtml(settings.shuffleTarget);
+      }, settings.replaceTime * (index))
+    })
   }
 
-  // play Index
-  let j
-  for (j = 0; j < long; j++) {
-    data.playIndex.push(j);
+  const setShortArray = (shortArray, longArray) => {
+    let shortI = 0
+    let i
+    for (i = 0; i < longArray.length; i++) {
+      if (data.replaceIndex[shortI] !== i) {
+        shortArray.splice(i, 0, '');
+      } else {
+        shortI++
+      }
+    }
   }
-  if (initData.isRandom) {
-    data.playIndex = shuffleArray(data.playIndex);
+
+  const getRandomIndex = (short, long) => {
+    // replace Index
+    let check
+    let replaceI
+    let lastReplaceI = -1;
+    let i
+    for (i = 0; i < short; i++) {
+      check = long - (short - i) - (lastReplaceI + 1)
+      replaceI = Math.floor(Math.random() * check);
+      lastReplaceI = (lastReplaceI + 1) + replaceI;
+      data.replaceIndex.push(lastReplaceI);
+    }
+
+    // play Index
+    let j
+    for (j = 0; j < long; j++) {
+      data.playIndex.push(j);
+    }
+    if (settings.isReplacedRandomly) {
+      data.playIndex = shuffleArray(data.playIndex);
+    }
   }
-}
 
-const repeatAction = (shortArray, longArray) => {
-  // console.log("repeatAction");
-  getRandomIndex(shortArray.length, longArray.length);
-  setShortArray(shortArray, longArray);
-  replaceText();
-}
+  const repeatAction = (shortArray, longArray) => {
+    getRandomIndex(shortArray.length, longArray.length);
+    setShortArray(shortArray, longArray);
+    replaceText();
+  }
 
-const compareLength = (item) => {
-  // console.log("compareLength");
-  data.nextText = item.split('');
-  const nextLength = data.nextText.length;
-  const currentLength = data.currentSay.length;
+  const compareLength = (item) => {
+    data.nextText = item.split('');
+    const nextLength = data.nextText.length;
+    const currentLength = data.currentSay.length;
+    
+    if (nextLength < currentLength) {
+      repeatAction(data.nextText, data.currentSay);
+      data.totalReplaceTime = currentLength * settings.replaceTime;
+    } else {
+      repeatAction(data.currentSay, data.nextText);
+      data.totalReplaceTime = nextLength * settings.replaceTime;
+    }
+  }
+
+  let setAutoTime;
+  let sayIndex = 1;  // next index
+  const playAuto = () => {
+    clearTimeout(setAutoTime)
+    setAutoTime = setTimeout(() => {
+      play();
+      playAuto();
+    }, data.totalReplaceTime + (settings.stayTime));
+  }
+  const play = () => {
+    data.replaceIndex = [];
+    data.playIndex = [];
+    compareLength(data.say[sayIndex]); // item은 say에서 next item
+    sayIndex = sayIndex == data.say.length - 1 ? sayIndex = 0 : sayIndex + 1;
+  }
+
+  const shuffleArray = (array) => {
+    let currentIndex = array.length;
+    let randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  const insertHtml = (target) => {
+    let htmlText = ''
+    data.currentSay.forEach(el => {
+      htmlText = htmlText + el
+    });
+    target.innerHTML = htmlText
+  }
   
-  if (nextLength < currentLength) {
-    repeatAction(data.nextText, data.currentSay);
-    data.totalReplaceTime = currentLength * data.replaceTime;
-  } else {
-    repeatAction(data.currentSay, data.nextText);
-    data.totalReplaceTime = nextLength * data.replaceTime;
+  ShuffleText.prototype = {
+    playAuto,
+    play,
+    clear: () => {
+      clearTimeout(setReplaceTime)
+      clearTimeout(setAutoTime)
+    }
   }
-}
 
-let setAutoTime;
-let sayIndex = 1;  // next index
-const playAutoType = () => {
-  // console.log("playAutoType -------");
-  setAutoTime = setTimeout(() => {
-    play();
-    playAutoType();
-  }, data.totalReplaceTime + (data.stayTime));
-}
-const play = () => {
-  // console.log("data", data);
-  data.replaceIndex = [];
-  data.playIndex = [];
-  compareLength(data.say[sayIndex]); // item은 say에서 next item
-  sayIndex = sayIndex == data.say.length - 1 ? sayIndex = 0 : sayIndex + 1;
-}
-
-const shuffleArray = (array) => {
-  // console.log("shuffleArray");
-  let currentIndex = array.length;
-  let randomIndex;
-  while (0 !== currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
-  return array;
-}
-
-const shuffleTarget = document.querySelector(".shuffle-target")
-const insertHtml = () => {
-  // console.log("insertHtml");
-  let htmlText = ''
-  data.currentSay.forEach(el => {
-    htmlText = htmlText + el
-  });
-  shuffleTarget.innerHTML = htmlText
-}
-
-const init = () => {
-  data.say = initData.isDisorder ? shuffleArray(initData.textArray) : initData.textArray;
-  data.txtLength = data.say.map(item => item.length);
-
-  data.say[0].split('').forEach(txt => {
-    data.currentSay.push(`<span>${txt}</span>`);
-  })
-  data.stayTime = initData.stayTime;
-  data.replaceTime = initData.replaceTime;
-  
-  // console.log("data ---", data);
-
-  if (initData.playType == 'auto') {
-    playAutoType();
-  } else if (initData.playType == 'hover') {
-    initData.hoverTarget.addEventListener('mouseenter', e => {
-      console.log("mouseenter", data.nextText);
-      e.preventDefault();
-      play();
-    });
-    initData.hoverTarget.addEventListener('mouseleave', e => {
-      console.log("mouseleave");
-      e.preventDefault();
-      play();
-    });
-  } else if (initData.playType == 'click') {
-    initData.hoverTarget.addEventListener('click', e => {
-      e.preventDefault();
-      play();
-    });
-  }
-  insertHtml();
-}
-
-init();
+  return ShuffleText;
+}));
